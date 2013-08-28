e83f6ebf2ddbb2e7856664ec1ede8c03e6ac8433
Osei-Bonsu Christian <christian.kacob@gmail.com>
2013-08-28 02:07:37 +0000

    Finished with all configurations and is able to display map

--- main/java/com/fourapps/cabkonnect/Home.java
+++ main/java/com/fourapps/cabkonnect/Home.java
@@ -4,18 +4,24 @@ import android.app.Activity;
 import android.os.Bundle;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
+import android.support.v4.app.FragmentActivity;
 
 /**
  * Created by nanakay on 6/6/13.
  */
-public class Home extends Activity {
+public class Home extends FragmentActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
 
-//        MapView mv = (MapView) findViewById(R.id.mapview);
+ //    MapView mv = (MapView) findViewById(R.id.mapview);
 //        mv.setBuiltInZoomControls(true);
     }
+
+//    @Override
+//    protected boolean isRouteDisplayed() {
+//        return false;
+//    }
 
 }
